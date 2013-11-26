05435d154f39edd6e0f19299a570cfddaad18c5f
Alex Suraci and Onsi Fakhouri <pair+alex+onsi@pivotallabs.com>
2013-11-25 17:57:50 -0800

    first pass at simulator

--- simulator/simulator.go
+++ simulator/simulator.go
@@ -1,43 +1,53 @@
 package simulator
 
 import (
+	"fmt"
 	"github.com/cloudfoundry/hm9000/models"
 	"github.com/cloudfoundry/hm9000/testhelpers/appfixture"
 	"github.com/cloudfoundry/hm9000/testhelpers/desiredstateserver"
 	"github.com/cloudfoundry/yagnats"
+	"math/rand"
 )
 
 type Simulator struct {
 	MaxDEACapacity int
 
-	FractionOfNewAppsStartingPerHB float64
-	FractionOfAppsStoppingPerHB    float64
-	FractionOfAppsThatAreCrashed   float64
+	NumberOfNewAppsStartingPerHB int
+	FractionOfAppsThatAreCrashed float64
 
 	HeartbeatIntervalInSeconds int
 	AdvertiseIntervalInSeconds int
 
-	Apps           []*appfixture.AppFixture
-	SecondsElapsed int
+	Apps             []*appfixture.AppFixture
+	AppInstanceState map[string]models.InstanceState
+	SecondsElapsed   int
 
-	deas map[string][]*appfixture.AppFixture
+	DEAs                 map[string][]*appfixture.AppFixture
+	DEAHeartbeatSchedule map[string]int
+	DEAAdvertiseSchedule map[string]int
 
+	randomizer         *rand.Rand
 	messageBus         yagnats.NATSClient
 	desiredStateServer desiredstateserver.DesiredStateServerInterface
 }
 
-func New(numberOfApps int, initialDEACount int, messageBus yagnats.NATSClient, desiredStateServer desiredstateserver.DesiredStateServerInterface) *Simulator {
+func New(numberOfApps int, initialDEACount int, randomizer *rand.Rand, messageBus yagnats.NATSClient, desiredStateServer desiredstateserver.DesiredStateServerInterface) *Simulator {
 	simulator := &Simulator{
-		MaxDEACapacity:                 100,
-		FractionOfNewAppsStartingPerHB: 0.01,
-		FractionOfAppsStoppingPerHB:    0.01,
-		FractionOfAppsThatAreCrashed:   0.1,
-		HeartbeatIntervalInSeconds:     10,
-		AdvertiseIntervalInSeconds:     5,
-		messageBus:                     messageBus,
-		desiredStateServer:             desiredStateServer,
-		Apps:                           []*appfixture.AppFixture{},
-		deas:                           map[string][]*appfixture.AppFixture{},
+		MaxDEACapacity:               100,
+		NumberOfNewAppsStartingPerHB: 10,
+		FractionOfAppsThatAreCrashed: 0.1,
+		HeartbeatIntervalInSeconds:   10,
+		AdvertiseIntervalInSeconds:   5,
+
+		randomizer:         randomizer,
+		messageBus:         messageBus,
+		desiredStateServer: desiredStateServer,
+
+		Apps:                 []*appfixture.AppFixture{},
+		AppInstanceState:     map[string]models.InstanceState{},
+		DEAs:                 map[string][]*appfixture.AppFixture{},
+		DEAHeartbeatSchedule: map[string]int{},
+		DEAAdvertiseSchedule: map[string]int{},
 	}
 
 	simulator.prepare(numberOfApps, initialDEACount)
@@ -45,23 +55,83 @@ func New(numberOfApps int, initialDEACount int, messageBus yagnats.NATSClient, d
 }
 
 func (s *Simulator) TickOneSecond() {
+	for dea, schedule := range s.DEAAdvertiseSchedule {
+		if schedule == s.SecondsElapsed%s.AdvertiseIntervalInSeconds {
+			s.emitAdvertiseFor(dea)
+		}
+	}
 
+	for dea, schedule := range s.DEAHeartbeatSchedule {
+		if schedule == s.SecondsElapsed%s.HeartbeatIntervalInSeconds {
+			s.emitHeartbeatFor(dea)
+		}
+	}
+
+	s.SecondsElapsed++
+
+	if s.SecondsElapsed%s.HeartbeatIntervalInSeconds == 0 {
+		for i := 0; i < s.NumberOfNewAppsStartingPerHB; i++ {
+			s.buildNewApp()
+		}
+		s.populateDesiredState()
+	}
 }
 
 func (s *Simulator) prepare(numberOfApps, initialDEACount int) {
 	for i := 0; i < initialDEACount; i++ {
-		s.deas[models.Guid()] = make([]*appfixture.AppFixture, 0)
+		s.buildDEA()
 	}
 	for i := 0; i < numberOfApps; i++ {
 		s.buildNewApp()
 	}
+	s.populateDesiredState()
+}
+
+func (s *Simulator) populateDesiredState() {
+	desiredStates := make([]models.DesiredAppState, len(s.Apps))
+	for i, app := range s.Apps {
+		desiredStates[i] = app.DesiredState(1)
+	}
+	s.desiredStateServer.SetDesiredState(desiredStates)
+}
+
+func (s *Simulator) emitAdvertiseFor(dea string) {
+	s.messageBus.Publish("dea.advertise", []byte(fmt.Sprintf(`{"dea":"%s"}`, dea)))
+}
+
+func (s *Simulator) emitHeartbeatFor(dea string) {
+	hb := models.Heartbeat{
+		DeaGuid:            dea,
+		InstanceHeartbeats: make([]models.InstanceHeartbeat, len(s.DEAs[dea])),
+	}
+
+	for i, app := range s.DEAs[dea] {
+		hb.InstanceHeartbeats[i] = app.InstanceAtIndex(0).Heartbeat()
+		hb.InstanceHeartbeats[i].State = s.AppInstanceState[app.AppGuid]
+	}
+
+	s.messageBus.Publish("dea.heartbeat", hb.ToJSON())
+}
+
+func (s *Simulator) buildDEA() string {
+	guid := models.Guid()
+	s.DEAs[guid] = make([]*appfixture.AppFixture, 0)
+	s.DEAHeartbeatSchedule[guid] = s.randomizer.Intn(s.HeartbeatIntervalInSeconds)
+	s.DEAAdvertiseSchedule[guid] = s.randomizer.Intn(s.AdvertiseIntervalInSeconds)
+	return guid
 }
 
 func (s *Simulator) buildNewApp() {
 	app := appfixture.NewAppFixture()
-	winner := models.Guid()
+	if s.randomizer.Float64() < s.FractionOfAppsThatAreCrashed {
+		s.AppInstanceState[app.AppGuid] = models.InstanceStateCrashed
+	} else {
+		s.AppInstanceState[app.AppGuid] = models.InstanceStateRunning
+	}
+
+	winner := ""
 	lowball := 1000000
-	for dea, apps := range s.deas {
+	for dea, apps := range s.DEAs {
 		if len(apps)+1 > s.MaxDEACapacity {
 			continue
 		}
@@ -71,7 +141,11 @@ func (s *Simulator) buildNewApp() {
 		}
 	}
 
+	if winner == "" {
+		winner = s.buildDEA()
+	}
+
 	app.DeaGuid = winner
-	s.deas[winner] = append(s.deas[winner], &app)
+	s.DEAs[winner] = append(s.DEAs[winner], &app)
 	s.Apps = append(s.Apps, &app)
 }
--- simulator/simulator_test.go
+++ simulator/simulator_test.go
@@ -1,12 +1,15 @@
 package simulator_test
 
 import (
+	"github.com/cloudfoundry/hm9000/models"
 	. "github.com/cloudfoundry/hmperformance/simulator"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"time"
 
 	"github.com/cloudfoundry/hm9000/testhelpers/desiredstateserver"
 	"github.com/cloudfoundry/yagnats/fakeyagnats"
+	"math/rand"
 	"sort"
 )
 
@@ -15,12 +18,14 @@ var _ = Describe("Simulator", func() {
 		sim                *Simulator
 		messageBus         *fakeyagnats.FakeYagnats
 		desiredStateServer *desiredstateserver.DesiredStateServer
+		randomizer         *rand.Rand
 	)
 
 	BeforeEach(func() {
 		desiredStateServer = desiredstateserver.NewDesiredStateServer()
 		messageBus = fakeyagnats.New()
-		sim = New(100, 8, messageBus, desiredStateServer)
+		randomizer = rand.New(rand.NewSource(time.Now().Unix()))
+		sim = New(100, 8, randomizer, messageBus, desiredStateServer)
 	})
 
 	Describe("Preparing the simulator", func() {
@@ -32,22 +37,36 @@ var _ = Describe("Simulator", func() {
 			Ω(sim.SecondsElapsed).Should(Equal(0))
 		})
 
+		It("should populate the desired state server", func() {
+			Ω(desiredStateServer.Apps).Should(HaveLen(100))
+			for _, desired := range desiredStateServer.Apps {
+				Ω(desired.NumberOfInstances).Should(Equal(1))
+				Ω(desired.State).Should(Equal(models.AppStateStarted))
+				Ω(desired.PackageState).Should(Equal(models.AppPackageStateStaged))
+			}
+		})
+
 		Context("when the # of requested apps fit within the initial DEA count", func() {
+			It("should generate the requested number of DEAs", func() {
+				Ω(sim.DEAs).Should(HaveLen(8))
+			})
+
 			It("should spread the apps evenly among the DEAs", func() {
 				deas := make(map[string]int)
 				for _, app := range sim.Apps {
 					deas[app.DeaGuid] += 1
 				}
 				Ω(deas).Should(HaveLen(8))
-				for _, appCount := range deas {
+				for dea, appCount := range deas {
 					Ω(appCount).Should(BeNumerically("~", 13, 1))
+					Ω(sim.DEAs[dea]).Should(HaveLen(appCount))
 				}
 			})
 		})
 
 		Context("when the # of requested apps need more DEAs to fit", func() {
 			It("should fill up the DEAs then generate new ones as needed", func() {
-				sim = New(250, 2, messageBus, desiredStateServer)
+				sim = New(250, 2, randomizer, messageBus, desiredStateServer)
 				deas := make(map[string]int)
 				for _, app := range sim.Apps {
 					deas[app.DeaGuid] += 1
@@ -62,6 +81,86 @@ var _ = Describe("Simulator", func() {
 				sort.Ints(appCounts)
 				Ω(appCounts).Should(Equal([]int{50, 100, 100}))
 			})
+		})
+	})
+
+	Describe("Ticking the simulator", func() {
+		BeforeEach(func() {
+			sim.NumberOfNewAppsStartingPerHB = 0 //turn this off to make these tests simpler
+		})
+
+		It("should emit a heartbeat for each DEA by every heartbeat interval", func() {
+			Ω(sim.HeartbeatIntervalInSeconds).Should(Equal(10))
+			for i := 0; i < sim.HeartbeatIntervalInSeconds; i++ {
+				sim.TickOneSecond()
+			}
+			Ω(messageBus.PublishedMessages["dea.heartbeat"]).Should(HaveLen(8))
+
+			heartbeats := map[string]models.Heartbeat{}
+			for _, message := range messageBus.PublishedMessages["dea.heartbeat"] {
+				heartbeat, err := models.NewHeartbeatFromJSON(message.Payload)
+				Ω(err).ShouldNot(HaveOccured())
+				heartbeats[heartbeat.DeaGuid] = heartbeat
+			}
+
+			for dea, apps := range sim.DEAs {
+				Ω(heartbeats[dea].InstanceHeartbeats).Should(HaveLen(len(apps)))
+			}
+		})
+
+		It("should emit an advertise for each DEA by every advertise interval", func() {
+			Ω(sim.AdvertiseIntervalInSeconds).Should(Equal(5))
+			for i := 0; i < sim.AdvertiseIntervalInSeconds; i++ {
+				sim.TickOneSecond()
+			}
+			Ω(messageBus.PublishedMessages["dea.advertise"]).Should(HaveLen(8))
+		})
+
+		It("should report a consistent portion of apps as crashed", func() {
+			Ω(sim.FractionOfAppsThatAreCrashed).Should(Equal(0.1))
+
+			for i := 0; i < sim.HeartbeatIntervalInSeconds; i++ {
+				sim.TickOneSecond()
+			}
+
+			appStates := map[models.InstanceState]int{}
+			total := 0
+			for _, message := range messageBus.PublishedMessages["dea.heartbeat"] {
+				heartbeat, err := models.NewHeartbeatFromJSON(message.Payload)
+				Ω(err).ShouldNot(HaveOccured())
+				for _, instanceHeartbeat := range heartbeat.InstanceHeartbeats {
+					appStates[instanceHeartbeat.State]++
+					total++
+				}
+			}
+
+			Ω(total).Should(Equal(100))
+			Ω(appStates[models.InstanceStateCrashed]).Should(BeNumerically("~", 10, 8), "This may fail.. non-deterministally, but it should fail *very* infrequently.")
+			Ω(appStates[models.InstanceStateCrashed] + appStates[models.InstanceStateRunning]).Should(Equal(total))
+		})
+	})
+
+	Describe("Adding new apps with time", func() {
+		It("should add apps at the specified rate", func() {
+			Ω(sim.NumberOfNewAppsStartingPerHB).Should(Equal(10))
+			for i := 0; i < 12; i++ {
+				messageBus.Reset()
+				for j := 0; j < sim.HeartbeatIntervalInSeconds; j++ {
+					sim.TickOneSecond()
+				}
+			}
+
+			numApps := 100 + 12*10
+
+			Ω(sim.Apps).Should(HaveLen(numApps))
+			Ω(sim.DEAs).Should(HaveLen(8), "Shouldn't have needed any additional DEAs")
+			Ω(desiredStateServer.Apps).Should(HaveLen(numApps))
+		})
+	})
+
+	Describe("Scenarios", func() {
+		Describe("Removing an entire DEA", func() {
+
 		})
 	})
 })
